ae6b5e24ab5d557f3100728c48aad39ea91ab437
unnamedunknownusername <109242984+unnamedunknownusername@users.noreply.github.com>
2023-03-02 22:19:33 +0200

    Update generate.jl
    fix typo

--- generate/generate.jl
+++ generate/generate.jl
@@ -105,7 +105,7 @@ show_section_number = false
 # ╔═╡ 01a2336a-5c04-4d5a-bb0b-a9c704517dbf
 pages = [
 	(page = "/thanks/", title = "Acknowledgements"),
-	(page = "/errors/", title = "Did you spot a error?"),
+	(page = "/errors/", title = "Did you spot an error?"),
 	#(page = "/installation/", title = "Software installation"),
 	#(page = "/cheatsheets/", title = "Cheatsheets")
 ]
