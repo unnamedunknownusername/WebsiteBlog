a8c5e6a7c08ad69ca322db98a2821d14132440cb
unnamedunknownusername <109242984+unnamedunknownusername@users.noreply.github.com>
2023-03-04 20:39:59 +0200

    Create FormattingFunctions.jl
    add common formatting functions

---
+++ notebooks/FormattingFunctions.jl
@@ -0,0 +1,62 @@
+### A Pluto.jl notebook ###
+# v0.19.22
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 7e0348f6-2284-475d-a101-c6842b05db4c
+
+
+# ╔═╡ e1386345-4493-4467-b634-8da57532fb11
+
+
+# ╔═╡ 6752f134-9f36-4646-a914-a916f757719b
+
+
+# ╔═╡ 9f1effe5-f766-4f2e-aa4b-3967261fd84b
+
+
+# ╔═╡ a3ca2950-4bc0-45ed-9652-24ed589e1dfd
+paragraphJustifiedStyling()=html"""
+<style>
+p {
+  text-align: justify;
+  text-justify: inter-word;
+}
+</style>
+"""
+
+# ╔═╡ 1e1faf71-1ba3-413b-abf2-e6af90cd5e1f
+md"""
+# Tests 
+"""
+
+# ╔═╡ edfd62bc-1222-482f-9318-d5c3102687d8
+
+
+# ╔═╡ f5d8a360-b9f9-45a1-a202-029157b3c985
+
+
+# ╔═╡ eb4af17a-b40f-4751-9f35-341d658fe150
+md"""
+# Import Pacakges
+"""
+
+# ╔═╡ f333aa56-2cbc-4caf-8fde-3515bbb2d977
+using PlutoUI
+
+# ╔═╡ 79a6c1e6-b22e-4674-ba0a-8862336864e3
+
+
+# ╔═╡ Cell order:
+# ╠═7e0348f6-2284-475d-a101-c6842b05db4c
+# ╠═e1386345-4493-4467-b634-8da57532fb11
+# ╠═6752f134-9f36-4646-a914-a916f757719b
+# ╠═9f1effe5-f766-4f2e-aa4b-3967261fd84b
+# ╠═a3ca2950-4bc0-45ed-9652-24ed589e1dfd
+# ╠═1e1faf71-1ba3-413b-abf2-e6af90cd5e1f
+# ╠═edfd62bc-1222-482f-9318-d5c3102687d8
+# ╠═f5d8a360-b9f9-45a1-a202-029157b3c985
+# ╠═eb4af17a-b40f-4751-9f35-341d658fe150
+# ╠═f333aa56-2cbc-4caf-8fde-3515bbb2d977
+# ╠═79a6c1e6-b22e-4674-ba0a-8862336864e3
